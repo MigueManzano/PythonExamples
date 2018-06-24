4e640ec4623a10771901b11afb06937122c72d62
ing.dev.manzano <ing.dev.manzano@gmail.com>
2018-06-23 23:04:45 -0500

    calculadora final

--- calculadora.py
+++ calculadora.py
@@ -3,32 +3,33 @@ ciclo = 'y'
 while ciclo == 'y':
     numero1 = float(input("Ingrese un valor numerico entero: "))
     numero2 = float(input("Ingrese un segundo valor numerico entero: "))
-    opcion = input("""" Ingrese la opcion numerica
+    opcion = int(input("""" Ingrese una opcion numerica
     1.- Sumar
     2.- Restar
     3.- Multiplicar
     4.- Dividir 
-    5.- Modulo o residuo""")
+    5.- Modulo o residuo\n
+     """))
 
     if opcion == 1:
         print('{} + {} = {}'.format(numero1, numero2, numero1+numero2,))
-        ciclo = input('Desea hacer otra operacion nueva? y/n')
+        ciclo = input('Desea hacer otra operacion nueva? y/n\n')
 
     elif opcion == 2:
         print('{} - {} = {}'.format(numero1, numero2, numero1 - numero2))
-        ciclo = input('Desea hacer otra operacion nueva? y/n')
+        ciclo = input('Desea hacer otra operacion nueva? y/n\n')
 
     elif opcion == 3:
         print('{} * {} = {}'.format(numero1, numero2, numero1 * numero2))
-        ciclo = input('Desea hacer otra operacion nueva? y/n')
+        ciclo = input('Desea hacer otra operacion nueva? y/n\n')
 
     elif opcion == 4:
         print('{} / {} = {}'.format(numero1, numero2, numero1 / numero2))
-        ciclo = input('Desea hacer otra operacion nueva? y/n')
+        ciclo = input('Desea hacer otra operacion nueva? y/n\n')
 
     elif opcion == 5:
         print('{} % {} = {}'.format(numero1, numero2, numero1 % numero2))
-        ciclo = input('Desea hacer otra operacion nueva? y/n')
+        ciclo = input('Desea hacer otra operacion nueva? y/n\n')
 
     else:
         print('No es una opcion valida.')
